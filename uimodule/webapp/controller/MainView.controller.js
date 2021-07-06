09beb6549c1a7d54cabd3611e7aebe60d18c5c34
Escoriza <agustin.e.escoriza@accenture.com>
2021-07-06 11:09:01 -0300

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/YPF/mapApp/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.YPF.mapApp.controller.MainView", {});
+});
